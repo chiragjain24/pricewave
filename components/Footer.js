360232a07747e4711c581300891526cfdb63744e
chirag <chiragjain78382@gmail.com>
2024-09-22 22:19:03 +0530

    update

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <>
     <div className='text-center bg-gray-200 py-1'>All rights Reserved &copy; &nbsp; Created by: Chirag Jain</div>
+    <p>Hello there are you a Recruiter?</p>
     </>
   )
 }
